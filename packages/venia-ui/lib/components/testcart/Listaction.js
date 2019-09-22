7cd9090558e3f09f138db59cc25774f8d7cc58a6
antonioele <antonio.ele@tiscali.it>
2019-09-22 18:38:34 +0200

    add right div

--- packages/venia-ui/lib/components/testcart/Listaction.js
+++ packages/venia-ui/lib/components/testcart/Listaction.js
@@ -1,24 +1,27 @@
-import React , {Fragment} from 'react';
+import React , {Fragment,useState} from 'react';
 
 import defaultClasses from './Listaction.css';
 
 const Listaction =props=>{
+ const [evento,setEvento]=useState("CreateCart");
     console.log (`default class ${defaultClasses}`);
+    const Vistaevento=() =>{alert};
     const ls=["Createcart","additemtoCart","getCartdetails",
         "togglecart","removecart","getcartid","clearcartid","writeimagetocache"];
         const listItems = ls.map((number,index) =>
-        <li key={index} className={defaultClasses.root} >
-          <button className={defaultClasses.target}>{number.toUpperCase()}</button>
+        <li key={index}  >
+          <button onClick={Vistaevento}   className={defaultClasses.target}>{number.toUpperCase()}</button>
           </li>
       );
         return(
 
 <>
+<div className= {defaultClasses.root}>
 <ul>
 {listItems}
 </ul>
-
-
+<div> <h1>plutooooooooooooooooooooooooo</h1></div>
+</div>
 
 </>
 
